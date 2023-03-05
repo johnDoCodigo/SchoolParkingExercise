7014b463a8d36e0ab85f8f24ab307d7ec58d2d58
hicalonic <hicalonic@gmail.com>
2023-03-05 17:03:26 +0000

    3 questions done

--- java/mindswap/schoolParking/Main.java
+++ java/mindswap/schoolParking/Main.java
@@ -18,7 +18,7 @@ public class Main {
         Teacher t4 = new Teacher("Rodrigues", 25);
         Teacher t5 = new Teacher("Marques", 26);
         Teacher t6 = new Teacher("Machado", 27);
-        //createVehicles
+        //createVehicles with teachers
 
         Car car1 = new Car("44-ZZ-99", "BMW", t1);
         Car car2 = new Car("55-XX-88", "AUDI", t2);
@@ -26,6 +26,14 @@ public class Main {
         Bike bike1 = new Bike("88-AA-66", "YAMAHAA", t4);
         Bike bike2 = new Bike("77-BB-55", "HONDA", t5);
         Bike bike3 = new Bike("99-CC-44", "SUZUKI", t6);
+
+        // give cars to teachers
+        t1.setVehicle(car1);
+        t2.setVehicle(car2);
+        t3.setVehicle(car3);
+        t4.setVehicle(bike1);
+        t5.setVehicle(bike2);
+        t6.setVehicle(bike3);
 
         //createParkingSlots
 
@@ -35,14 +43,6 @@ public class Main {
         ParkingSlot p4 = new ParkingSlot();
         ParkingSlot p5 = new ParkingSlot();
         ParkingSlot p6 = new ParkingSlot();
-
-
-        t1.setVehicle(car1);
-        t2.setVehicle(car2);
-        t3.setVehicle(car3);
-        t4.setVehicle(bike1);
-        t5.setVehicle(bike2);
-        t6.setVehicle(bike3);
 
         //addVehicles
 
@@ -69,16 +69,11 @@ public class Main {
         em.persist(p5);
         em.persist(p6);
         em.getTransaction().commit();
-        //enter on carPark
+
+
         em.getTransaction().begin();
 
-        car3.setParkingSlot(p1);
-        car2.setParkingSlot(p2);
-        car1.setParkingSlot(p3);
-        bike1.setParkingSlot(p4);
-        bike2.setParkingSlot(p5);
-        bike3.setParkingSlot(p6);
-
+        //enter on carPark
         p1.addVehicle(car3);
         p2.addVehicle(car2);
         p3.addVehicle(car1);
@@ -86,8 +81,15 @@ public class Main {
         p5.addVehicle(bike2);
         p6.addVehicle(bike3);
 
-        em.getTransaction().commit();
+        //AssignSlots to vehicles
+        car3.setParkingSlot(p1);
+        car2.setParkingSlot(p2);
+        car1.setParkingSlot(p3);
+        bike1.setParkingSlot(p4);
+        bike2.setParkingSlot(p5);
+        bike3.setParkingSlot(p6);
 
+        em.getTransaction().commit();
 
          //a. listing all the vehicles
         em.getTransaction().begin();
@@ -120,7 +122,5 @@ public class Main {
                 .forEach(Teacher::print);
 
         em.getTransaction().commit();
-
-
     }
 }
